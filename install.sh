d53ef2fd5457ede065bfc4ec196963cec9f9129e
vipbeko <beko18@mailna.me>
2017-03-24 23:51:19 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+sudo apt-get update
+sudo apt-get update
+sudo apt-get update
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get upgrade
+sudo apt-get upgrade
+sudo apt-get upgrade
+sudo apt-get upgrade
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson* libpython-dev make unzip git redis-server g++ -y —force-yes
+sudo apt-get upgrade
+cd VIPTEAM
+sudo apt-get update
+chmod +x VIPTEAM
+sudo apt-get update
+./launch.sh install
+./launch.sh
+
+
+
+
+
+
+
+--install.sh with lua by VIP-TEAM
+—ch : @DEV_VIP_TEAM
+—dev : @ABOD_EVO
